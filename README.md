5ce161d9ce0131e54e7ff2f1617c2eea3d766115
trismagp <tris.magp@gmail.com>
2022-12-26 21:04:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Getting Started
+
+1. Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> git clone https://github.com/trismagp/tkinter_examples.git
+> cd tkinter_examples
+> pip install pytube thinker
+> python [app_file_name].py
+```
---
+++ youtube_download_app.py
@@ -0,0 +1,41 @@
+#https://www.pythontutorial.net/tkinter/tkinter-example/
+
+from pytube import YouTube
+import customtkinter
+
+
+
+
+def download_360p_mp4_videos(url: str, outpath: str = "./"):
+    yt = YouTube(url)
+    yt.streams.filter(file_extension="mp4").get_by_resolution("360p").download(outpath)
+    return yt.title
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Simple youtube downloader")
+        self.minsize(400, 300)
+        customtkinter.set_appearance_mode("dark")
+        self.entry = customtkinter.CTkEntry(master=self,
+                               placeholder_text="input youtube url",
+                               width=520,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10)
+        self.entry.pack(padx=20, pady=30)
+        self.button = customtkinter.CTkButton(master=self, text="download", command=self.button_callback)
+        self.button.pack(padx=5, pady=20)
+        self.textbox = customtkinter.CTkTextbox(master=self, width = 520)
+        self.textbox.pack( padx=5, pady=10)
+
+    def button_callback(self):
+        title = download_360p_mp4_videos(self.entry.get().strip())
+        self.textbox.insert("insert",  title + "downloaded\n")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
