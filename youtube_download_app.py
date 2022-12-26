329669b26470150968342747f3650fc2c4062eb0
trismagp <tris.magp@gmail.com>
2022-12-26 21:31:30 +0100

    delete url after download

--- youtube_download_app.py
+++ youtube_download_app.py
@@ -1,6 +1,7 @@
 #https://www.pythontutorial.net/tkinter/tkinter-example/
 
 from pytube import YouTube
+import tkinter as tk
 import customtkinter
 
 
@@ -27,13 +28,17 @@ class App(customtkinter.CTk):
                                corner_radius=10)
         self.entry.pack(padx=20, pady=30)
         self.button = customtkinter.CTkButton(master=self, text="download", command=self.button_callback)
-        self.button.pack(padx=5, pady=20)
+        self.button.pack(padx=5, pady=5)
         self.textbox = customtkinter.CTkTextbox(master=self, width = 520)
         self.textbox.pack( padx=5, pady=10)
 
     def button_callback(self):
-        title = download_360p_mp4_videos(self.entry.get().strip())
-        self.textbox.insert("insert",  title + "downloaded\n")
+        url = self.entry.get().strip()
+        self.entry.delete(0,tk.END)
+        self.entry.placeholder_text = "input youtube url"
+        
+        title = download_360p_mp4_videos(url)
+        self.textbox.insert("insert",  title + "==>downloaded\n")
 
 
 if __name__ == "__main__":
